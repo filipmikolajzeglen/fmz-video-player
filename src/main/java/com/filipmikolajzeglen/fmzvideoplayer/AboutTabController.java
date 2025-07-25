6d0c63b56a05c35b808ecac8a3cc7d65b67bc9aa
Filip Żegleń <filipmikolajzeglen@gmail.com>
2025-07-25 21:01:23 +0200

    FMZ-33 The first step with refactorization. Moved "About" Tab to AboutTabController and a related XML section moved from fmz-startup-config.fxml to tabs/about-tab.fxml

---
+++ src/main/java/com/filipmikolajzeglen/fmzvideoplayer/AboutTabController.java
@@ -0,0 +1,11 @@
+package com.filipmikolajzeglen.fmzvideoplayer;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class AboutTabController {
+
+   @FXML
+   private VBox aboutContent;
+
+}
